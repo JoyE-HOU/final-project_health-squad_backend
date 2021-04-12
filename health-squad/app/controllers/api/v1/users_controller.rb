ceb6901b415862146661de11f56950745889206f
Joy Evans <joyevans@me.com>
2021-04-12 09:48:41 -0500

    updates after meeting with Joe

--- health-squad/app/controllers/api/v1/users_controller.rb
+++ health-squad/app/controllers/api/v1/users_controller.rb
@@ -6,10 +6,10 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
+        @user = User.create!(user_params)
         if @user.valid?
             @token = encode_token(user_id: @user_id)
-            render json: { user: UserSerializer.new(@user) }, status: :created
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
